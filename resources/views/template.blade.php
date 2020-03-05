773686b3edae5ede09da3f38567881080069b376
Cristiam Herrera <cristiamherrera@gmail.com>
2020-03-05 19:39:51 -0400

    Se creo la rama develop

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,7 +7,7 @@
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                Aqui el contenido de muestra
+                Aqui el contenido de muestra para develop
             </div>
         </div>
     </div>
