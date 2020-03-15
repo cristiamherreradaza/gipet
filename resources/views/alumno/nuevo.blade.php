a29607ff6610318475e0bdef552912bfc8f0e053
Cristiam Herrera <cristiamherrera@gmail.com>
2020-03-14 20:23:15 -0400

    Se diseno el formulario de alumno

--- resources/views/alumno/nuevo.blade.php
+++ resources/views/alumno/nuevo.blade.php
@@ -14,62 +14,256 @@
             <div class="col-lg-12">
                 <div class="card card-outline-info">
                     <div class="card-header">
-                        <h4 class="mb-0 text-white">NUEVA CARRERA</h4>
+                        <h4 class="mb-0 text-white">NUEVO ALUMNO</h4>
                     </div>
                     <div class="card-body">
-                        <form>
-                            <div class="form-body">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nombre </label>
-                                            <input type="text" class="form-control" name="data[Alumno][nombre]" id="nombre">
-                                        </div>
+                        {{-- datos personales --}}
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card card-outline-warning">
+                                    <div class="card-header">
+                                        <h4 class="mb-0 text-white">DATOS PERSONALES</h4>
                                     </div>
+                                    <div class="card-body">
 
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nombre de la Carrera </label>
-                                            <input type="text" class="form-control" name="nombre" id="nombre">
-                                        </div>
-                                    </div>
+                                        <form>
+                                            <div class="form-body">
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Apellido Materno </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][apellido_paterno]" id="nombre">
+                                                        </div>
+                                                    </div>
 
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nombre de la Carrera </label>
-                                            <input type="text" class="form-control" name="nombre" id="nombre">
-                                        </div>
-                                    </div>
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Apellido Paterno </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][apellido_materno]" id="nombre">
+                                                        </div>
+                                                    </div>
 
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nombre de la Carrera </label>
-                                            <input type="text" class="form-control" name="nombre" id="nombre">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Nivel Formacion </label>
-                                            <input type="text" class="form-control" name="nivel" id="nivel">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group">
-                                            <label>Semestre </label>
-                                            <input type="text" class="form-control" name="semestre" id="semestre">
-                                        </div>
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Nombres </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Carnet </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][carnet]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Expedido </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][expedido]" id="nivel">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Genero </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][sexo]" id="nivel">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Email </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][email]" id="nivel">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Direccion </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][sexo]" id="nivel">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="form-group">
+                                                            <label>Genero </label>
+                                                            <input type="text" class="form-control"
+                                                                name="data[Persona][sexo]" id="nivel">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn waves-effect waves-light btn-block btn-success btnenviar">GUARDAR</button>
+                        </div>
+                        {{-- fin datos personales --}}
+
+                        {{-- datos profesionales --}}
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card card-outline-inverse">
+                                    <div class="card-header">
+                                        <h4 class="mb-0 text-white">DATOS PROFESIONALES</h4>
+                                    </div>
+                                    <div class="card-body">
+
+                                            <div class="form-body">
+                                                <div class="row">
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label>Trabaja </label>
+                                                            <select class="form-control">
+                                                                <option value="No">No</option>
+                                                                <option value="Si">Si</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Empresa </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-4">
+                                                        <div class="form-group">
+                                                            <label>Direccion </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Telefono </label>
+                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    
+                                                </div>
+                                                
+                                            </div>
+                                        
+                                    </div>
+                                </div>
                             </div>
-                        </form>
+                        </div>
+                        {{-- fin datos profesionales --}}
+
+                        {{-- referencias personales --}}
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card card-outline-success">
+                                    <div class="card-header">
+                                        <h4 class="mb-0 text-white">REFERENCIAS PERSONALES</h4>
+                                    </div>
+                                    <div class="card-body">
+
+                                            <div class="form-body">
+                                                <div class="row">
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Padre </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Padre </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Madre </label>
+                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Madre </label>
+                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                
+                                            </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- fin referencias personales --}}
+
+                        {{-- Carrera --}}
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card card-outline-info">
+                                    <div class="card-header">
+                                        <h4 class="mb-0 text-white">REFERENCIAS PERSONALES</h4>
+                                    </div>
+                                    <div class="card-body">
+
+                                            <div class="form-body">
+                                                <div class="row">
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Padre </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Padre </label>
+                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Madre </label>
+                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Madre </label>
+                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                
+                                            </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- fin Carrera --}}
+
+
                     </div>
+                    
                 </div>
             </div>
         </div>
