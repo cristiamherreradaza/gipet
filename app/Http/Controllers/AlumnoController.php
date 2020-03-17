261a0b06b0aa633138a05fb4ea18672f41a40703
Cristiam Herrera <cristiamherrera@gmail.com>
2020-03-17 10:06:50 -0400

    Se corrigio el error

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -2,18 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Turno;
+use App\Carrera;
+use App\Persona;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AlumnoController extends Controller
 {
     public function nuevo()
     {
-        return view('alumno/nuevo');
+        // $combo_carreras = DB::table('carreras');
+        $carreras = Carrera::where('borrado', NULL)->get();
+        $turnos = Turno::where('borrado', NULL)->get();
+        // dd($carreras);
+        return view('alumno/nuevo')->with(compact('carreras', 'turnos'));
     }
 
     public function guarda(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
+        $persona = new Persona();
+        $persona->apellido_paterno = $request->apellido_paterno;
+        $persona->apellido_materno = $request->apellido_materno;
+        $persona->nombres          = $request->nombres;
+        $persona->carnet           = $request->carnet;
+        $persona->expedido         = $request->expedido;
+        $persona->fecha_nacimiento = $request->fecha_nacimiento;
+        $persona->sexo             = $request->sexo;
+        $persona->telefono_celular = $request->telefono_celular;
+        $persona->email            = $request->email;
+        $persona->trabaja          = $request->trabaja;
+        $persona->empresa          = $request->empresa;
+        $persona->direcion_empresa = $request->direcion_empresa;
+        $persona->telefono_empresa = $request->telefono_empresa;
+        $persona->nombre_padre     = $request->nombre_padre;
+        $persona->celular_padre    = $request->celular_padre;
+        $persona->nombre_madre     = $request->nombre_madre;
+        $persona->celular_madre    = $request->celular_madre;
+        $persona->nombre_tutor     = $request->nombre_tutor;
+        $persona->telefono_tutor   = $request->telefono_tutor;
+        $persona->nombre_esposo    = $request->nombre_esposo;
+        $persona->telefono_esposo  = $request->telefono_esposo;
+        $persona->save();
     }
     public function ariel()
     {
--- resources/views/alumno/nuevo.blade.php
+++ resources/views/alumno/nuevo.blade.php
@@ -65,44 +65,66 @@
 
                                                     <div class="col">
                                                         <div class="form-group">
+                                                            <label>Fecha Nacimiento </label>
+                                                            <input type="date" class="form-control"
+                                                                name="fecha_nacimiento" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col">
+                                                        <div class="form-group">
                                                             <label>Expedido </label>
-                                                            <select class="form-control">
-                                                                <option value="No">No</option>
-                                                                <option value="Si">Si</option>
+                                                            <select name="expedido" class="form-control">
+                                                                <option value="La Paz">La Paz</option>
+                                                                <option value="Cochabamba">Cochabamba</option>
+                                                                <option value="Santa Cruz">Santa Cruz</option>
+                                                                <option value="Oruro">Oruro</option>
+                                                                <option value="Potosi">Potosi</option>
+                                                                <option value="Tarija">Tarija</option>
+                                                                <option value="Sucre">Sucre</option>
+                                                                <option value="Beni">Beni</option>
+                                                                <option value="Pando">Pando</option>
                                                             </select>
                                                         </div>
                                                     </div>
 
                                                 </div>
                                                 <div class="row">
-                                                    <div class="col">
-                                                        <div class="form-group">
-                                                            <label>Genero </label>
-                                                            <input type="text" class="form-control"
-                                                                name="data[Persona][sexo]" id="nivel">
-                                                        </div>
-                                                    </div>
-                                                    <div class="col">
+
+                                                    <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Email </label>
                                                             <input type="text" class="form-control"
-                                                                name="data[Persona][email]" id="nivel">
+                                                                name="email" id="nivel">
                                                         </div>
                                                     </div>
-                                                    <div class="col">
+
+                                                    <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Direccion </label>
                                                             <input type="text" class="form-control"
-                                                                name="data[Persona][sexo]" id="nivel">
+                                                                name="direccion" id="nivel">
                                                         </div>
                                                     </div>
-                                                    <div class="col">
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular </label>
+                                                            <input type="text" class="form-control"
+                                                                name="telefono_celular" id="nivel">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Genero </label>
-                                                            <input type="text" class="form-control"
-                                                                name="data[Persona][sexo]" id="nivel">
+                                                            <select name="sexo" class="form-control">
+                                                                <option value="Masculino">Masculino</option>
+                                                                <option value="Femenino">Femenino</option>
+                                                            </select>
                                                         </div>
                                                     </div>
+                                                    
                                                 </div>
 
                                             </div>
@@ -127,7 +149,7 @@
                                                     <div class="col-md-2">
                                                         <div class="form-group">
                                                             <label>Trabaja </label>
-                                                            <select class="form-control">
+                                                            <select class="form-control" name="trabaja">
                                                                 <option value="No">No</option>
                                                                 <option value="Si">Si</option>
                                                             </select>
@@ -137,21 +159,21 @@
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Nombre Empresa </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <input type="text" class="form-control" name="empresa" id="nombre">
                                                         </div>
                                                     </div>
 
                                                     <div class="col-4">
                                                         <div class="form-group">
                                                             <label>Direccion </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <input type="text" class="form-control" name="direccion_empresa" id="nombre">
                                                         </div>
                                                     </div>
 
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Telefono </label>
-                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                            <input type="text" class="form-control" name="telefono_empresa" id="nombre">
                                                         </div>
                                                     </div>
 
@@ -176,33 +198,64 @@
                                     <div class="card-body" style="background-color: #e3ffe3;">
 
                                             <div class="form-body">
-                                                <div class="row">
 
+                                                <div class="row">
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Nombre Padre </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <input type="text" class="form-control" name="nombre_padre" id="nombre">
                                                         </div>
                                                     </div>
 
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Celular Padre </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <input type="text" class="form-control" name="celular_padre" id="nombre">
                                                         </div>
                                                     </div>
 
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Nombre Madre </label>
-                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                            <input type="text" class="form-control" name="nombre_madre" id="nombre">
                                                         </div>
                                                     </div>
 
                                                     <div class="col-3">
                                                         <div class="form-group">
                                                             <label>Celular Madre </label>
-                                                            <input type="text" class="form-control" name="data[Persona][nombres]" id="nombre">
+                                                            <input type="text" class="form-control" name="celular_madre" id="nombre">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+
+                                                <div class="row">
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Tutor </label>
+                                                            <input type="text" class="form-control" name="nombre_tutor" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Tutor </label>
+                                                            <input type="text" class="form-control" name="telefono_tutor" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Nombre Esposo </label>
+                                                            <input type="text" class="form-control" name="nombre_esposo" id="nombre">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-3">
+                                                        <div class="form-group">
+                                                            <label>Celular Esposo </label>
+                                                            <input type="text" class="form-control" name="telefono_esposo" id="nombre">
                                                         </div>
                                                     </div>
                                                     
@@ -221,24 +274,32 @@
                             <div class="col-lg-12">
                                 <div class="card card-outline-info">
                                     <div class="card-header">
-                                        <h4 class="mb-0 text-white">REFERENCIAS PERSONALES</h4>
+                                        <h4 class="mb-0 text-white">CARRERA</h4>
                                     </div>
                                     <div class="card-body" style="background-color: #e6f4fa;">
 
                                             <div class="form-body">
                                                 <div class="row">
 
-                                                    <div class="col-3">
+                                                    <div class="col-md-2">
                                                         <div class="form-group">
-                                                            <label>Nombre Padre </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <label>Carrera </label>
+                                                            <select class="form-control" name="trabaja">
+                                                                @foreach ($carreras as $c)
+                                                                <option value="{{ $c->id }}">{{ $c->nombre }}</option>
+                                                                @endforeach
+                                                            </select>
                                                         </div>
                                                     </div>
 
-                                                    <div class="col-3">
+                                                    <div class="col-md-2">
                                                         <div class="form-group">
-                                                            <label>Celular Padre </label>
-                                                            <input type="text" class="form-control" name="data[Alumno][empresa]" id="nombre">
+                                                            <label>Turno </label>
+                                                            <select class="form-control" name="trabaja">
+                                                                @foreach ($turnos as $t)
+                                                                <option value="{{ $t->id }}">{{ $t->descripcion }}</option>
+                                                                @endforeach
+                                                            </select>
                                                         </div>
                                                     </div>
 
