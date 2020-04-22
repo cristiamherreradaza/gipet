e0664b9afe64420aea7eabe02ad3c761d36722a0
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-04-22 19:48:18 -0400

    Commit de prueba

--- routes/web.php
+++ routes/web.php
@@ -50,28 +50,30 @@ Route::get('persona/ajax_datos', 'PersonaController@ajax_datos');
 Route::get('persona/exportarexcel', 'PersonaController@exportarExcel')->name('personas.exportarexcel');
 
 
+Route::get('user/asignar', 'UserController@asignar');
+
 //NOTAS
 Route::get('nota/listado', 'NotaController@listado');
+
 Route::get('nota/detalle/{id}', 'NotaController@detalle');
-Route::post('nota/ajax_importar', 'NotaController@ajax_importar');
-Route::post('nota/importarexcel', 'NotaController@importarexcel');
-Route::get('user/asignar', 'UserController@asignar');
-Route::get('nota/actualizar', 'NotaController@actualizar');
+
 Route::get('nota/exportarexcel/{id}', 'NotaController@exportarexcel');
-Route::get('nota/index', 'NotaController@index');
-Route::get('nota/asignatura/{id}', 'NotaController@asignatura');
-Route::get('nota/show', 'NotaController@show');
-Route::get('nota/detalle', 'NotaController@detalle');
-Route::get('nota/show2', 'NotaController@show2');
-Route::get('nota/detalle2', 'NotaController@detalle2');
 
 Route::get('nota/segundoTurno/{id}', 'NotaController@segundoTurno');
-Route::get('nota/segundoTurnoActualizar', 'NotaController@segundoTurnoActualizar');
+
+Route::post('nota/actualizar', 'NotaController@actualizar');
+
+Route::post('nota/ajax_importar', 'NotaController@ajax_importar');
+
+Route::post('nota/segundoTurnoActualizar', 'NotaController@segundoTurnoActualizar');
 
 //NOTAS PROPUESTA
 Route::get('notaspropuesta/listado', 'NotasPropuestaController@listado');
-Route::get('notaspropuesta/actualizar', 'NotasPropuestaController@actualizar');
+
 Route::get('notaspropuesta/exportarexcel/{id}', 'NotasPropuestaController@exportarexcel');
+
+Route::post('notaspropuesta/actualizar', 'NotasPropuestaController@actualizar');
+
 Route::post('notaspropuesta/ajax_importar', 'NotasPropuestaController@ajax_importar');
 
 //MIGRACIONES
