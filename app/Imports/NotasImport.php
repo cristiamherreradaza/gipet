7932d1667222a4acbda2c3c73b8ae4826207c39a
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2020-04-25 15:45:01 -0400

    Se implemento mas validaciones en Notas y NotasPropuestas

--- app/Imports/NotasImport.php
+++ app/Imports/NotasImport.php
@@ -5,6 +5,7 @@ namespace App\Imports;
 use App\Nota;
 use App\Kardex;
 use App\Inscripcion;
+use App\NotasPropuesta;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class NotasImport implements ToModel
@@ -16,35 +17,47 @@ class NotasImport implements ToModel
     */
     public function model(array $row)
     {
-        
+        // Si el valor que esta en la posición 0 es numerico (id de la tabla notas)
         if( is_numeric($row[0]) ){
+            // Buscamos ese id en notas
             $nota = Nota::find($row[0]);
-            if($nota->persona->carnet == $row[2]){
-                $nota->nota_asistencia = $row[3];
-                $nota->nota_practicas = $row[4];
-                $nota->nota_puntos_ganados = $row[5];
-                $nota->nota_primer_parcial = $row[6];
-                $nota->nota_examen_final = $row[7];
-                $nota->nota_total = ($row[3]+$row[4]+$row[5]+$row[6]+$row[7]);
-                $nota->save();
-                //Registro en Inscripcion
-                $inscripcion = Inscripcion::where('asignatura_id', $nota->asignatura_id)
+            // Buscamos la Notapropuesta relacionada a esta Nota
+            $notapropuesta = NotasPropuesta::where('asignatura_id', $nota->asignatura_id)
                                         ->where('turno_id', $nota->turno_id)
-                                        ->where('persona_id', $nota->persona_id)
-                                        ->where('paralelo', $nota->paralelo) 
+                                        ->where('paralelo', $nota->paralelo)
                                         ->where('anio_vigente', $nota->anio_vigente)
+                                        ->where('borrado', NULL)
+                                        ->first();
+            // Si el valor que se encuentra en la BBDD es igual al valor del excel (carnet)
+            if($nota->persona->carnet == $row[2]){
+                // Si el valor de cada Nota no sobrepasa al de su NotaPropuesta
+                if($row[3] <= $notapropuesta->nota_asistencia && $row[4] <= $notapropuesta->nota_practicas && $row[5] <= $notapropuesta->nota_puntos_ganados && $row[6] <= $notapropuesta->nota_primer_parcial && $row[7] <= $notapropuesta->nota_examen_final){
+                    $nota->nota_asistencia = $row[3];
+                    $nota->nota_practicas = $row[4];
+                    $nota->nota_puntos_ganados = $row[5];
+                    $nota->nota_primer_parcial = $row[6];
+                    $nota->nota_examen_final = $row[7];
+                    $nota->nota_total = ($row[3]+$row[4]+$row[5]+$row[6]+$row[7]);
+                    $nota->save();
+                    // Registro en Inscripción
+                    $inscripcion = Inscripcion::where('asignatura_id', $nota->asignatura_id)
+                                            ->where('turno_id', $nota->turno_id)
+                                            ->where('persona_id', $nota->persona_id)
+                                            ->where('paralelo', $nota->paralelo) 
+                                            ->where('anio_vigente', $nota->anio_vigente)
+                                            ->firstOrFail();
+                    $inscripcion->nota = $nota->nota_total;
+                    $inscripcion->save();
+                    // Registro en Kardex
+                    if($nota->nota_total >= 61){
+                        // Procederemos a la busqueda en kardex y actualización
+                        $kardex = Kardex::where('persona_id', $nota->persona_id)
+                                        ->where('asignatura_id', $nota->asignatura_id)
                                         ->firstOrFail();
-                $inscripcion->nota = $nota->nota_total;
-                $inscripcion->save();
-                //Registro en Kardex
-                if($nota->nota_total >= 61){
-                    //procederemos a la busqueda en kardex y actualización
-                    $kardex = Kardex::where('persona_id', $nota->persona_id)
-                                    ->where('asignatura_id', $nota->asignatura_id)
-                                    ->firstOrFail();
-                    $kardex->aprobado = 'Si';
-                    $kardex->anio_aprobado = date('Y');
-                    $kardex->save();
+                        $kardex->aprobado = 'Si';
+                        $kardex->anio_aprobado = date('Y');
+                        $kardex->save();
+                    }
                 }
             }
         }
--- app/Imports/NotasPropuestasImport.php
+++ app/Imports/NotasPropuestasImport.php
@@ -14,16 +14,22 @@ class NotasPropuestasImport implements ToModel
     */
     public function model(array $row)
     {
+        // Si el valor que esta en la posición 0 es numerico (id de la tabla notas_propuestas)
         if( is_numeric($row[0]) ){
+            // Buscamos ese id en notas_propuestas
             $notapropuesta = NotasPropuesta::find($row[0]);
+            // Si el valor que se encuentra en la BBDD es igual al valor del excel (codigo_asignatura)
             if($notapropuesta->asignatura->codigo_asignatura == $row[1]){
-                $notapropuesta->nota_asistencia = $row[3];
-                $notapropuesta->nota_practicas = $row[4];
-                $notapropuesta->nota_puntos_ganados = $row[5];
-                $notapropuesta->nota_primer_parcial = $row[6];
-                $notapropuesta->nota_examen_final = $row[7];
-                $notapropuesta->fecha = date('Y-m-d H:i:s');
-                $notapropuesta->save();
+                // Si la suma total de los valores es igual a 100
+                if($row[3]+$row[4]+$row[5]+$row[6]+$row[7] == 100){
+                    $notapropuesta->nota_asistencia = $row[3];
+                    $notapropuesta->nota_practicas = $row[4];
+                    $notapropuesta->nota_puntos_ganados = $row[5];
+                    $notapropuesta->nota_primer_parcial = $row[6];
+                    $notapropuesta->nota_examen_final = $row[7];
+                    $notapropuesta->fecha = date('Y-m-d H:i:s');
+                    $notapropuesta->save();
+                }
             }
         }
     }
