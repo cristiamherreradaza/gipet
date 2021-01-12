e0d8f087bf4398566b4ddb99c83bfd1673f09ede
Ariel Fernandez <arielfernandez.rma7@gmail.com>
2021-01-12 15:31:59 -0400

    Se corrigio error en AppAsignatura

--- app/Asignatura.php
+++ app/Asignatura.php
@@ -60,10 +60,4 @@ class Asignatura extends Model
     {
         return $this->hasMany('App\SegundosTurno');
     }
-
-    public function resolucion()
-    {
-        return $this->belongsTo('App\Resolucione', 'resolucion_id');
-    }
-
 }
