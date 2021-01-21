84c05b031f88be831c3d1134a46c140798529b70
cristiam <cristiamherrera@gmail.com>
2021-01-21 09:29:23 -0400

    Se cambio la distancia de la impresion del contrato

--- resources/views/pdf/contrato.blade.php
+++ resources/views/pdf/contrato.blade.php
@@ -23,7 +23,7 @@
             /* border-collapse: collapse; */
             background-color: #fff;
             position: absolute;
-            top: 520px;
+            top: 420px;
             left: 150px;
             padding: 20px;
             border: 1px solid black;
