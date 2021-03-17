339f88e32333403049bb1ca3d006d8d9838e2c8b
Cristiam Herrera <cristiamherrera@gmail.com>
2021-03-17 10:15:37 -0400

    Se quito las librerias

--- resources/views/carrera/ajax_combo_materias.blade.php
+++ resources/views/carrera/ajax_combo_materias.blade.php
@@ -1,16 +1,5 @@
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/select2/dist/css/select2.min.css') }}">
-
 <select class="form-control" name="fp_materia" style="width: 100%">
     @foreach ($asignaturas as $asignatura)
         <option value="{{ $asignatura->id }}">{{ $asignatura->nombre }} ({{ $asignatura->sigla }})</option>
     @endforeach
 </select>
-
-<script src="{{ asset('assets/libs/select2/dist/js/select2.full.min.js') }}" type="text/javascript"></script>
-<script type="text/javascript">
-    $(function () {
-        $(".select2").select2({
-            dropdownParent: $('#modal_prerequisitos')
-        });
-    });
-</script>
