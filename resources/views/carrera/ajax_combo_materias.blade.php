2e591d5d4022b8069548a0a039f51230c8ca61e2
Cristiam Herrera <cristiamherrera@gmail.com>
2021-03-16 23:29:01 -0400

    Se corrigio los prerequisitos

--- resources/views/carrera/ajax_combo_materias.blade.php
+++ resources/views/carrera/ajax_combo_materias.blade.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/select2/dist/css/select2.min.css') }}">
 
-<select class="select2" name="fp_materia" style="width: 100%">
+<select class="form-control" name="fp_materia" style="width: 100%">
     @foreach ($asignaturas as $asignatura)
         <option value="{{ $asignatura->id }}">{{ $asignatura->nombre }} ({{ $asignatura->sigla }})</option>
     @endforeach
 </select>
 
-<script src="{{ asset('assets/plugins/select2/dist/js/select2.full.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/libs/select2/dist/js/select2.full.min.js') }}" type="text/javascript"></script>
 <script type="text/javascript">
     $(function () {
         $(".select2").select2({
