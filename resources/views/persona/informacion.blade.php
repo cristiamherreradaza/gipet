826c8905527332e9ad1a4ca414ffa711bdfb42ce
Cristiam Herrera <cristiamherrera@gmail.com>
2021-05-12 08:32:25 -0400

    Se transformo a espanol el datatable de materias

--- resources/views/persona/informacion.blade.php
+++ resources/views/persona/informacion.blade.php
@@ -419,7 +419,7 @@
                 </div>
                 <div class="tab-pane" id="profile1">
                     <div class="table-responsive m-t-40">
-                        <table class="table table-striped table-bordered no-wrap table-hover text-inputs-searching">
+                        <table id="tabla-materias" class="table table-striped table-bordered no-wrap table-hover">
                             <thead>
                                 <tr>
                                     <th>Carrera</th>
@@ -610,6 +610,59 @@
 
     // Funcion que establece la configuracion para el datatable
     $(function () {
+
+        $('#tabla-materias tfoot th').each(function() {
+            var title = $(this).text();
+            $(this).html('<input type="text" class="form-control" placeholder="Busca ' + title + '" />');
+        });
+
+        // DataTable
+        var tableSearching = $('#tabla-materias').DataTable({
+            "order": [[ 6, 'desc' ]],
+            "language": {
+                "sProcessing": "Procesando...",
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+                },
+                "oAria": {
+                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                },
+                "buttons": {
+                "copy": "Copiar",
+                "colvis": "Visibilidad"
+                }
+            }
+
+        });
+
+        // Apply the search
+        tableSearching.columns().every(function() {
+            var that = this;
+
+            $('input', this.footer()).on('keyup change', function() {
+                if (that.search() !== this.value) {
+                    that
+                        .search(this.value)
+                        .draw();
+                }
+            });
+        });
+
         // Setup - add a text input to each footer cell
         /*$('#tabla-materias thead th').each( function () {
             var title = $(this).text();
@@ -634,11 +687,6 @@
             }
         });*/
 
-        /*$('#tabla-materias').DataTable({
-            language: {
-                url: '{{ asset('datatableEs.json') }}'
-            },
-        });*/
     });
 
     function actualizaPerfilEstudiante()
