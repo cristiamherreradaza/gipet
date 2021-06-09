38a1d04d0c0b9c5ae59fc19994324e815c6e52ef
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-06-09 09:55:34 -0400

    se corrigio el datatable de calificaciones bimestral

--- resources/views/lista/centralizadorBimestral.blade.php
+++ resources/views/lista/centralizadorBimestral.blade.php
@@ -1,14 +1,21 @@
-<table class="table table-striped table-bordered no-wrap table-hover dataTable no-footer">
-    <tr>
-        <td>No.</td>
-        <td colspan="3">APELLIDOS Y NOMBRES</td>
-        <td>ASISTENCIA</td>
-        <td>TRABAJOS PRACTICOS</td>
-        <td>EXAMEN PARCIAL</td>
-        <td>EXAMEN FINAL</td>
-        <td>PUNTOS GANADOS</td>
-        <td>BIM</td>
-    </tr>
+<link href="{{ asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css') }}" rel="stylesheet">
+
+<table id="tabla-alumnos-bimestral" class="table table-striped table-bordered table-hover table-striped">
+    <thead>
+        <tr>
+            <th>No.</th>
+            <th>AP PATERNO</th>
+            <th>AP MATERNO</th>
+            <th>NOMBRES</th>
+            <th>ASISTENCIA</th>
+            <th>TRABAJOS PRACTICOS</th>
+            <th>EXAMEN PARCIAL</th>
+            <th>EXAMEN FINAL</th>
+            <th>PUNTOS GANADOS</th>
+            <th>BIM</th>
+        </tr>
+    </thead>
+    <tbody>
     @foreach ($alumnos as $key => $a)
     <tr>
         <td>{{ ++$key }}</td>
@@ -23,5 +30,19 @@
         <td>{{ intval($a->nota_total) }}</td>
     </tr>
     @endforeach
-
+    </tbody>
 </table>
+
+<script src="{{ asset('assets/libs/datatables/media/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('dist/js/pages/datatable/custom-datatable.js') }}"></script>
+<script>
+    $(function () {
+        $('#tabla-alumnos-bimestral').DataTable({
+            iDisplayLength: 10,
+            language: {
+                url: '{{ asset('datatableEs.json') }}'
+            },
+        });
+
+    });
+</script>
