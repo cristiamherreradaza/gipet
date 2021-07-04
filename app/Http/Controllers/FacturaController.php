18ef58e38c546b70e4b60e65d497e259567fb161
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-07-04 19:57:27 -0400

    Se adicono la libreria para facturar

--- app/Http/Controllers/FacturaController.php
+++ app/Http/Controllers/FacturaController.php
@@ -9,6 +9,7 @@ use App\Persona;
 use App\Servicio;
 use App\CarrerasPersona;
 use App\DescuentosPersona;
+use CodigoControlV7;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
