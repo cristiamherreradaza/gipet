9d97eb6c03741f548a8acb6991a9048c9a925624
Joel Flores Quispe <jjjoelcito123@gmail.com>
2021-12-12 11:04:17 -0400

    Se agrego los cambios

--- app/Imports/NotasImport.php
+++ app/Imports/NotasImport.php
@@ -30,89 +30,98 @@ class NotasImport implements ToModel, WithStartRow
         // calculamos el total de la nota del bimestre
         $total = $row[4] + $row[5] + $row[6] + $row[7] + $row[8];
 
-        $notapropuesta = NotasPropuesta::where('asignatura_id', $nota->asignatura_id)
+        
+        if($nota){
+
+            $notapropuesta = NotasPropuesta::where('asignatura_id', $nota->asignatura_id)
                                         ->where('turno_id', $nota->turno_id)
                                         ->where('paralelo', $nota->paralelo)
                                         ->where('anio_vigente', $nota->anio_vigente)
                                         ->first();
 
-        $registraNota = Nota::where('asignatura_id', $nota->asignatura_id)
-                                ->where('turno_id', $nota->turno_id)
-                                ->where('persona_id', $nota->persona_id)
-                                ->where('paralelo', $nota->paralelo)
-                                ->where('anio_vigente', $nota->anio_vigente)
-                                ->where('trimestre', $row[3])
-                                ->first();
 
-        $registraNota->nota_asistencia       = $row[4];
-        $registraNota->nota_practicas        = $row[5];
-        $registraNota->nota_primer_parcial   = $row[6];
-        $registraNota->nota_examen_final     = $row[7];
-        $registraNota->nota_puntos_ganados   = $row[8];      //extras
-        $registraNota->nota_total            = $total;
+            $registraNota = Nota::where('asignatura_id', $nota->asignatura_id)
+                                    ->where('turno_id', $nota->turno_id)
+                                    ->where('persona_id', $nota->persona_id)
+                                    ->where('paralelo', $nota->paralelo)
+                                    ->where('anio_vigente', $nota->anio_vigente)
+                                    ->where('trimestre', $row[3])
+                                    ->first();
 
-        if($registraNota->finalizado != 'Si'){
-            $registraNota->save();
-        }
+            $registraNota->nota_asistencia       = $row[4];
+            $registraNota->nota_practicas        = $row[5];
+            $registraNota->nota_primer_parcial   = $row[6];
+            $registraNota->nota_examen_final     = $row[7];
+            $registraNota->nota_puntos_ganados   = $row[8];      //extras
+            $registraNota->nota_total            = $total;
 
-        if($row[3] == 2){
-
-            $notaPrimerBimestre = Nota::where('asignatura_id', $nota->asignatura_id)
-                                        ->where('turno_id', $nota->turno_id)
-                                        ->where('persona_id', $nota->persona_id)
-                                        ->where('paralelo', $nota->paralelo)
-                                        ->where('anio_vigente', $nota->anio_vigente)
-                                        ->where('trimestre', 1)
-                                        ->first();
-
-            $sumaNotas  = $notaPrimerBimestre->nota_total+$total;
-            $promedio = $sumaNotas/2;
-
-            $datosAsignatura = Asignatura::find($nota->asignatura_id);
-
-            if($promedio >= $datosAsignatura->resolucion->nota_aprobacion){
-
-                $aprobo = 'Si';
-
-                $carrerasPersona = CarrerasPersona::where('persona_id', $nota->persona_id)
-                                                ->where('carrera_id', $nota->carrera_id)
-                                                ->where('turno_id', $nota->turno_id)
-                                                ->where('gestion', $nota->gestion)
-                                                ->where('paralelo', $nota->paralelo)
-                                                ->where('anio_vigente', $nota->anio_vigente)
-                                                ->first();
-
-                if($carrerasPersona->estado == null){
-                    
-                    $modificaEstado = CarrerasPersona::find($carrerasPersona->id);
-                    $modificaEstado->estado = 'APROBO';
-                    $modificaEstado->save();
-                }
-
-            }else{
-                $aprobo = null;
-                $carrerasPersona = CarrerasPersona::where('persona_id', $nota->persona_id)
-                                                ->where('carrera_id', $nota->carrera_id)
-                                                ->where('turno_id', $nota->turno_id)
-                                                ->where('gestion', $nota->gestion)
-                                                ->where('paralelo', $nota->paralelo)
-                                                ->where('anio_vigente', $nota->anio_vigente)
-                                                ->first();
-
-                $modificaEstado = CarrerasPersona::find($carrerasPersona->id);
-                // dd($modificaEstado);
-                // validamos si el alumno esta con estado de ABANDONO , ABANDONO TEMPORAL o CONGELADO NO SE EDITE EL ESTADO CON LA QUE ESTA
-                if($modificaEstado->estado != 'ABANDONO' && $modificaEstado->estado != 'ABANDONO TEMPORAL' && $modificaEstado->estado != 'CONGELADO'){
-                    $modificaEstado->estado = 'REPROBO';
-                }
-                $modificaEstado->save();                
+            if($registraNota->finalizado != 'Si'){
+                $registraNota->save();
             }
 
-            $inscripcion = Inscripcione::find($notaPrimerBimestre->inscripcion_id);
-            $inscripcion->nota = $promedio;
-            $inscripcion->aprobo = $aprobo;
-            $inscripcion->save();
+            if($row[3] == 2){
+
+                $notaPrimerBimestre = Nota::where('asignatura_id', $nota->asignatura_id)
+                                            ->where('turno_id', $nota->turno_id)
+                                            ->where('persona_id', $nota->persona_id)
+                                            ->where('paralelo', $nota->paralelo)
+                                            ->where('anio_vigente', $nota->anio_vigente)
+                                            ->where('trimestre', 1)
+                                            ->first();
+
+                $sumaNotas  = $notaPrimerBimestre->nota_total+$total;
+                $promedio = $sumaNotas/2;
+
+                $datosAsignatura = Asignatura::find($nota->asignatura_id);
+
+                if($promedio >= $datosAsignatura->resolucion->nota_aprobacion){
+
+                    $aprobo = 'Si';
+
+                    $carrerasPersona = CarrerasPersona::where('persona_id', $nota->persona_id)
+                                                    ->where('carrera_id', $nota->carrera_id)
+                                                    ->where('turno_id', $nota->turno_id)
+                                                    ->where('gestion', $nota->gestion)
+                                                    ->where('paralelo', $nota->paralelo)
+                                                    ->where('anio_vigente', $nota->anio_vigente)
+                                                    ->first();
+                    if($carrerasPersona){
+
+                        if($carrerasPersona->estado == null){
+                            
+                            $modificaEstado = CarrerasPersona::find($carrerasPersona->id);
+                            $modificaEstado->estado = 'APROBO';
+                            $modificaEstado->save();
+                        }
+                    }
+
+
+                }else{
+                    $aprobo = null;
+                    $carrerasPersona = CarrerasPersona::where('persona_id', $nota->persona_id)
+                                                    ->where('carrera_id', $nota->carrera_id)
+                                                    ->where('turno_id', $nota->turno_id)
+                                                    ->where('gestion', $nota->gestion)
+                                                    ->where('paralelo', $nota->paralelo)
+                                                    ->where('anio_vigente', $nota->anio_vigente)
+                                                    ->first();
+
+                    $modificaEstado = CarrerasPersona::find($carrerasPersona->id);
+                    // dd($modificaEstado);
+                    // validamos si el alumno esta con estado de ABANDONO , ABANDONO TEMPORAL o CONGELADO NO SE EDITE EL ESTADO CON LA QUE ESTA
+                    if($modificaEstado->estado != 'ABANDONO' && $modificaEstado->estado != 'ABANDONO TEMPORAL' && $modificaEstado->estado != 'CONGELADO'){
+                        $modificaEstado->estado = 'REPROBO';
+                    }
+                    $modificaEstado->save();                
+                }
+
+                $inscripcion = Inscripcione::find($notaPrimerBimestre->inscripcion_id);
+                $inscripcion->nota = $promedio;
+                $inscripcion->aprobo = $aprobo;
+                $inscripcion->save();
+            }
         }
+
     }
 
     public function startRow(): int
